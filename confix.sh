a5c3dbbb44442d83f176a6f79bff8f3337b6dfb3
GunGZN <86140154+GunGZN@users.noreply.github.com>
2021-10-02 05:27:09 +0700

    Update confix.sh

--- confix.sh
+++ confix.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo -e "************************************************************" | lolcat
-echo -e "                                           ลิ้งไฟล์ต่างๆ"     
+echo -e "                          ลิ้งไฟล์ต่างๆ"     
 echo -e "************************************************************" | lolcat
 echo -e "TCP conf ลิงค์​   :  http://$IP:81/TCP.ovpn"
 echo -e "UDP conf ลิงค์​   :  http://$IP:81/UDP.ovpn"
 echo -e "SSL conf ลิงค์​   :  http://$IP:81/SSL.ovpn"
-echo -e "true-nopro conf ลิงค์​   :  http://: 27.254.163.51:81/true-nopro.ovpn"
-echo -e "true-fbgaming ลิงค์​   :  http://$IP:81/true-fbgaming.ovpn"
+echo -e "  nopro  ลิงค์​   :  http://$IP:81/true-nopro.ovpn"
+echo -e "fbgaming ลิงค์​   :  http://$IP:81/true-fbgaming.ovpn"
 echo -e "************************************************************" | lolcat
