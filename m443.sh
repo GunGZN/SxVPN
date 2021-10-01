ff9504c082c85861d78fb6efbf626c651460c651
GunGZN <86140154+GunGZN@users.noreply.github.com>
2021-10-02 05:48:45 +0700

    Add files via upload

---
+++ m443.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+grep -E "^duplicate-cn" /etc/openvpn/443.conf >/dev/null
+ if [ $? -eq 0 ]; then
+ clear
+cr
+echo " ┈╭━━━━━━━━━━━━━━━━━━━━━━╮"
+echo " ┈┣ ♻️♻️ สถานะปัจจุบันเชื่อมได้ไม่จำกัดเครื่อง ♻️♻️"
+echo " ┈┣━━━━━━━━━━━━━━━━━━━━━━╯ "
+read -p " ┈┣ เปลี่ยนเป็น1ยุสเซอ ต่อ1เครื่องหรือไม่ Y/N :" selet
+if [[ "$selet" = "Y" || "$selet" = "y" ]]; then
+smilevpn="/duplicate-cn/d";
+sed -i $smilevpn /etc/openvpn/443.conf
+service openvpn restart -q > /dev/null 2>&1
+clear
+cr
+echo " ┈╭━━━━━━━━━━━━━━━━━━━━━━╮"
+echo " ┈┣ ♻️♻️ สถานะเปลี่ยนเป็น 1ยุสเซอ ต่อ1เครื่อง ♻️♻️"                             
+echo " ┈╰━━━━━━━━━━━━━━━━━━━━━━╯ "
+exit
+fi
+clear
+cr
+echo " ┈╭━━━━━━━━━━━━━━━━━━━━━━╮"
+echo " ┈┣ ♻️♻️ สถานะปัจจุบันเชื่อมได้ไม่จำกัดเครื่อง ♻️♻️"
+echo " ┈╰━━━━━━━━━━━━━━━━━━━━━━╯ "
+exit
+ else
+ clear
+cr
+echo " ┈╭━━━━━━━━━━━━━━━━━━━━━━╮"
+echo " ┈┣ ♻️♻️ สถานะปัจจุบัน 1ยุสเซอ ต่อ1เครื่อง ♻️♻️"
+echo " ┈┣━━━━━━━━━━━━━━━━━━━━━━╯ "
+read -p " ┈┣ เปลี่ยนเป็นไม่จำกัดเครื่องหรือไม่ Y/N :" selet
+if [[ "$selet" = "Y" || "$selet" = "y" ]]; then
+echo "duplicate-cn" >> /etc/openvpn/443.conf
+service openvpn restart -q > /dev/null 2>&1
+clear
+cr
+echo " ┈╭━━━━━━━━━━━━━━━━━━━━━━╮"
+echo " ┈┣ ♻️♻️ สถานะเปลี่ยนเป็นเชื่อมได้ไม่จำกัดเครื่อง ♻️♻️"
+echo " ┈╰━━━━━━━━━━━━━━━━━━━━━━╯ "
+exit
+fi
+clear
+cr
+echo " ┈╭━━━━━━━━━━━━━━━━━━━━━━╮"
+echo " ┈┣ ♻️♻️ สถานะปัจจุบัน 1ยุสเซอ ต่อ1เครื่อง ♻️♻️"
+echo " ┈╰━━━━━━━━━━━━━━━━━━━━━━╯ "
+exit
+fi
