53316eae99787e2da5344f1340bd5de09e6e880c
GunGZN <86140154+GunGZN@users.noreply.github.com>
2021-10-02 05:54:03 +0700

    Update and rename menuX to menuX.sh

--- menuX.sh
+++ menuX.sh
@@ -4,9 +4,10 @@ cd /usr/bin
 wget -q -O 02 "https://raw.githubusercontent.com/GunGZN/SxVPN/main/delet.sh"
 wget -q -O 11 "https://raw.githubusercontent.com/GunGZN/SxVPN/main/confix.sh"
 wget -q -O 12 "https://raw.githubusercontent.com/GunGZN/SxVPN/main/setspeed.sh"
-
+wget -q -O 13 "https://raw.githubusercontent.com/GunGZN/SxVPN/main/m443.sh"
 echo "30 3 * * 0 root /sbin/reboot" > /etc/cron.d/reboot
 
 chmod +x 02
 chmod +x 11
 chmod +x 12
+chmod +x 13
